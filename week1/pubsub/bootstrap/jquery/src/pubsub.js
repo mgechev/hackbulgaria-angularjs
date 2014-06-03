fc159983142d69274c5f41b2a4a16ef852799dec
mgechev <mgechev@gmail.com>
2014-06-03 20:47:22 +0300

    Add solutions

--- week1/pubsub/bootstrap/jquery/src/pubsub.js
+++ week1/pubsub/bootstrap/jquery/src/pubsub.js
@@ -1,14 +1,1 @@
-/* global jQuery */
 
-var pubsub = {};
-
-(function (ps, $) {
-  'use strict';
-  var temp = $({});
-  ps.publish = function (evnt, data) {
-    temp.trigger(evnt, data);
-  };
-  ps.subscibe = function (evnt, fn) {
-    temp.on(evnt, fn);
-  };
-}(pubsub, jQuery));
