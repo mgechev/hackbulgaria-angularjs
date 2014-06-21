586a0ba60aa4c94325d070acfa0cbc13ca2861ea
mgechev <mgechev@gmail.com>
2014-06-21 13:06:31 +0300

    Add extra digest call

--- week2/angularjs-databinding/solution/src/directives/ngl-model.js
+++ week2/angularjs-databinding/solution/src/directives/ngl-model.js
@@ -1,8 +1,10 @@
 Provider.directive('ngl-model', function () {
+  'use strict';
   return {
     link:  function (el, scope, exp) {
       el.onkeyup = function () {
         scope[exp] = el.value;
+        scope.$digest();
       };
       scope.$watch(exp, function (val) {
         el.value = val;
