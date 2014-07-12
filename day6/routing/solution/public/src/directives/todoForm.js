1ca1b5da7378f0aceb87e727e25a30188a549d64
mgechev <mgechev@gmail.com>
2014-07-12 15:33:21 +0300

    Change in the directive

--- day6/routing/solution/public/src/directives/todoForm.js
+++ day6/routing/solution/public/src/directives/todoForm.js
@@ -9,6 +9,7 @@ TodoApp.directive('todoForm', function () {
       save: '&'
     },
     link: function (scope) {
+      scope.todo.until = new Date(scope.todo.until);
       scope.saveData = function () {
         scope.todo.until = scope.todoDate + '/' + scope.todoTime;
         scope.save(scope.todo);
